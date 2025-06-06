b3ee7ff14e74f34b41fecb9ece9beb30a41b5884
Luizustavo <luizg.ss@outlook.com>
2025-06-06 14:28:57 -0300

    update API base URL to localhost:3001

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // passando o caminho da api
 const api = axios.create({
-    baseURL:'http://localhost:3000'
+    baseURL:'http://localhost:3001'
 })
 
 export default api
