ce3a96d77c7fb5c20186bdb4cc9ff5fedb8df880
Luizustavo <luizg.ss@outlook.com>
2025-06-06 14:32:38 -0300

    update API base URL to srv574063.hstgr.cloud

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // passando o caminho da api
 const api = axios.create({
-    baseURL:'http://localhost:3001'
+    baseURL:'http://srv574063.hstgr.cloud:3001/'
 })
 
 export default api
