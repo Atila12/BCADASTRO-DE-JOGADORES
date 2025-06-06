ee21ac19cf5a949104b437d490a9159c2930618c
Luizustavo <luizg.ss@outlook.com>
2025-06-06 14:25:17 -0300

    update API base URL to use host.docker.internal for Docker compatibility

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // passando o caminho da api
 const api = axios.create({
-    baseURL:'http://host.docker.internal:3001'
+    baseURL:'http://localhost:3000'
 })
 
 export default api
